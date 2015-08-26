0b18faf4071eee648543afc831f91a2a269990b2
Pathoscross <pathoscross@gmail.com>
2015-08-27 00:21:16 +0800

    first
    cs260

---
+++ [cs260]20150825/[exsimple01]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _exsimple01_
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student aStudent = new Student();
+            aStudent.Getdata();
+            aStudent.ShowData();
+        }
+    }
+}
---
+++ [cs260]20150825/[exsimple01]/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _exsimple01_
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string phone { get; set; }
+
+        public void Getdata()
+        {
+            Console.Write("Enter Your Name :");
+            this.Name = Console.ReadLine();
+            Console.Write("Enter Your Email :");
+            this.Email = Console.ReadLine();
+            Console.Write("Enter Your Phone :");
+            this.phone = Console.ReadLine();
+        }
+
+        public void ShowData()
+        {
+            string data = string.Format("姓名: {0}\nEmail: {1}\nPhone: {2}", this.Name, this.Email, this.Email);
+            Console.WriteLine(data);
+        }
+    }
+}
